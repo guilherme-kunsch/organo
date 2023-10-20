af60715cdbb50a2c16b9e139af0b57a63378d27f
guilherme-kunsch <guilherme.kunsch@outlook.com>
2023-10-19 21:29:48 -0300

    adicionando validação no form

--- src/components/Formulario/Index.js
+++ src/components/Formulario/Index.js
@@ -13,17 +13,21 @@ const Formulario = () => {
     'TI'
   ]
     
+  const aoEnviar = (evento) => {
+    evento.preventDefault();
+  }
+
 
   return (
     <section className="formulario">
-      <form>
+      <form onSubmit={aoEnviar}>
         <div className='logo'>
           <img src='../imagens/download.png'/>
         </div>
         <h2>Preencha os dados para criar o card do colaborador.</h2>
-        <CampoTexto label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
-        <CampoTexto label="Imagem" placeholder="Caminho da imagem" />
+        <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
+        <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
+        <CampoTexto obrigatorio={true} label="Imagem" placeholder="Caminho da imagem" />
         <ListaSuspensa label="Times" itens={times} />
         <Botao>
           Criar card
